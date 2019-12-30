3ae2e711d24f14e936472dd81485976cc4e6c794
wormogig <wormogig@mail.ru>
2019-12-30 12:50:59 +0300

    project start structure

---
+++ webapp/src/main/java/wormogig/TaskManagerApp.java
@@ -0,0 +1,12 @@
+package wormogig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerApp.class);
+    }
+}
